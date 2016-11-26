570705237e8c376a2fed02a63360aed2d213b727
Mitchell <Mitchell.J.Sarmento@gmail.com>
2016-11-25 18:00:26 -0800

    Created SectionHeader class to improve code modularity.

--- app/src/main/java/com/sarmento/mitchell/gradesaver2/activities/SectionActivity.java
+++ app/src/main/java/com/sarmento/mitchell/gradesaver2/activities/SectionActivity.java
@@ -15,6 +15,7 @@ import com.sarmento.mitchell.gradesaver2.adapters.AssignmentAdapter;
 import com.sarmento.mitchell.gradesaver2.dialogs.AssignmentDialogFragment;
 import com.sarmento.mitchell.gradesaver2.model.Academics;
 import com.sarmento.mitchell.gradesaver2.model.Section;
+import com.sarmento.mitchell.gradesaver2.views.SectionHeader;
 
 import java.util.Locale;
 
@@ -23,6 +24,7 @@ public class SectionActivity extends AppCompatActivity {
     private int sectionPosition;
     private AssignmentAdapter adapter;
     private Section section;
+    private SectionHeader header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +37,8 @@ public class SectionActivity extends AppCompatActivity {
         section = Academics.getInstance().getCurrentTerms().get(termPosition)
                 .getSections().get(sectionPosition);
 
-        setHeader(section);
+        header = (SectionHeader) findViewById(R.id.header_section);
+        header.init(section);
 
         RecyclerView assignments = (RecyclerView) findViewById(R.id.assignments);
         assignments.setLayoutManager(new LinearLayoutManager(this));
@@ -51,77 +54,7 @@ public class SectionActivity extends AppCompatActivity {
 
     public void updateList() {
         adapter.notifyDataSetChanged();
-        setHeader(section);
-    }
-
-    private void setHeader(Section section) {
-        SparseArray<Double> gradeThresholds = section.getGradeThresholds();
-
-        TextView gradeThresholdA = (TextView) findViewById(R.id.grade_threshold_a);
-        TextView gradeThresholdB = (TextView) findViewById(R.id.grade_threshold_b);
-        TextView gradeThresholdC = (TextView) findViewById(R.id.grade_threshold_c);
-        TextView gradeThresholdD = (TextView) findViewById(R.id.grade_threshold_d);
-        TextView gradeThresholdF = (TextView) findViewById(R.id.grade_threshold_f);
-
-        gradeThresholdA.setText(getString(R.string.a) + " " +
-                gradeThresholds.get(Section.HIGH_A) + " - " + gradeThresholds.get(Section.LOW_A));
-        gradeThresholdB.setText(getString(R.string.b) + " " +
-                gradeThresholds.get(Section.HIGH_B) + " - " + gradeThresholds.get(Section.LOW_B));
-        gradeThresholdC.setText(getString(R.string.c) + " " +
-                gradeThresholds.get(Section.HIGH_C) + " - " + gradeThresholds.get(Section.LOW_C));
-        gradeThresholdD.setText(getString(R.string.d) + " " +
-                gradeThresholds.get(Section.HIGH_D) + " - " + gradeThresholds.get(Section.LOW_D));
-        gradeThresholdF.setText(getString(R.string.f) + " " +
-                gradeThresholds.get(Section.HIGH_F) + " - " + gradeThresholds.get(Section.LOW_F));
-
-        SparseArray<Double> assignmentWeights = section.getAssignmentWeights();
-        SparseArray<Double> scores            = section.getScores();
-        SparseArray<Double> maxScores         = section.getMaxScores();
-
-        TextView scoreHomework = (TextView) findViewById(R.id.score_homework);
-        TextView scoreQuiz     = (TextView) findViewById(R.id.score_quiz);
-        TextView scoreMidterm  = (TextView) findViewById(R.id.score_midterm);
-        TextView scoreFinal    = (TextView) findViewById(R.id.score_final);
-        TextView scoreProject  = (TextView) findViewById(R.id.score_project);
-        TextView scoreOther    = (TextView) findViewById(R.id.score_other);
-
-        String scoreString;
-
-        scoreString = section.scoreToString(scores.get(Section.HOMEWORK),
-                maxScores.get(Section.HOMEWORK), Section.HOMEWORK);
-        scoreHomework.setText(scoreString + "/" + assignmentWeights.get(Section.HOMEWORK) + "%");
-
-        scoreString = section.scoreToString(scores.get(Section.QUIZ),
-                maxScores.get(Section.QUIZ), Section.QUIZ);
-        scoreQuiz.setText(scoreString + "/" + assignmentWeights.get(Section.QUIZ) + "%");
-
-        scoreString = section.scoreToString(scores.get(Section.MIDTERM),
-                maxScores.get(Section.MIDTERM), Section.MIDTERM);
-        scoreMidterm.setText(scoreString + "/" + assignmentWeights.get(Section.MIDTERM) + "%");
-
-        scoreString = section.scoreToString(scores.get(Section.FINAL),
-                maxScores.get(Section.FINAL), Section.FINAL);
-        scoreFinal.setText(scoreString + "/" + assignmentWeights.get(Section.FINAL) + "%");
-
-        scoreString = section.scoreToString(scores.get(Section.PROJECT),
-                maxScores.get(Section.PROJECT), Section.PROJECT);
-        scoreProject.setText(scoreString + "/" + assignmentWeights.get(Section.PROJECT) + "%");
-
-        scoreString = section.scoreToString(scores.get(Section.OTHER),
-                maxScores.get(Section.OTHER), Section.OTHER);
-        scoreOther.setText(scoreString + "/" + assignmentWeights.get(Section.OTHER) + "%");
-
-        TextView grade = (TextView) findViewById(R.id.grade);
-
-        double scorePercent = section.getTotalScore() / section.getMaxScore() * 100;
-
-        if (Double.isNaN(scorePercent)) {
-            scoreString = "";
-        } else {
-            scoreString = String.format(Locale.getDefault(), "%.2f", scorePercent) + "% " +
-                    section.getGrade();
-        }
-        grade.setText(getText(R.string.total) + " " + scoreString);
+        header.update(section);
     }
 
     @Override
--- app/src/main/java/com/sarmento/mitchell/gradesaver2/views/SectionHeader.java
+++ app/src/main/java/com/sarmento/mitchell/gradesaver2/views/SectionHeader.java
@@ -1,8 +1,105 @@
 package com.sarmento.mitchell.gradesaver2.views;
 
-/**
- * Created by Mitchell on 11/25/2016.
- */
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class SectionHeader {
+import com.sarmento.mitchell.gradesaver2.R;
+import com.sarmento.mitchell.gradesaver2.model.Section;
+
+import java.util.Locale;
+
+public class SectionHeader extends LinearLayout {
+    private Context context;
+    private TextView gradeThresholdA;
+    private TextView gradeThresholdB;
+    private TextView gradeThresholdC;
+    private TextView gradeThresholdD;
+    private TextView gradeThresholdF;
+    private TextView scoreHomework;
+    private TextView scoreQuiz;
+    private TextView scoreMidterm;
+    private TextView scoreFinal;
+    private TextView scoreProject;
+    private TextView scoreOther;
+    private TextView grade;
+
+    public SectionHeader(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        inflate(context, R.layout.header_section, this);
+        gradeThresholdA = (TextView) findViewById(R.id.grade_threshold_a);
+        gradeThresholdB = (TextView) findViewById(R.id.grade_threshold_b);
+        gradeThresholdC = (TextView) findViewById(R.id.grade_threshold_c);
+        gradeThresholdD = (TextView) findViewById(R.id.grade_threshold_d);
+        gradeThresholdF = (TextView) findViewById(R.id.grade_threshold_f);
+        scoreHomework   = (TextView) findViewById(R.id.score_homework);
+        scoreQuiz       = (TextView) findViewById(R.id.score_quiz);
+        scoreMidterm    = (TextView) findViewById(R.id.score_midterm);
+        scoreFinal      = (TextView) findViewById(R.id.score_final);
+        scoreProject    = (TextView) findViewById(R.id.score_project);
+        scoreOther      = (TextView) findViewById(R.id.score_other);
+        grade           = (TextView) findViewById(R.id.grade);
+    }
+
+    public void init(Section section) {
+        SparseArray<Double> gradeThresholds = section.getGradeThresholds();
+        gradeThresholdA.setText(context.getString(R.string.a) + " " +
+                gradeThresholds.get(Section.HIGH_A) + " - " + gradeThresholds.get(Section.LOW_A));
+        gradeThresholdB.setText(context.getString(R.string.b) + " " +
+                gradeThresholds.get(Section.HIGH_B) + " - " + gradeThresholds.get(Section.LOW_B));
+        gradeThresholdC.setText(context.getString(R.string.c) + " " +
+                gradeThresholds.get(Section.HIGH_C) + " - " + gradeThresholds.get(Section.LOW_C));
+        gradeThresholdD.setText(context.getString(R.string.d) + " " +
+                gradeThresholds.get(Section.HIGH_D) + " - " + gradeThresholds.get(Section.LOW_D));
+        gradeThresholdF.setText(context.getString(R.string.f) + " " +
+                gradeThresholds.get(Section.HIGH_F) + " - " + gradeThresholds.get(Section.LOW_F));
+
+        update(section);
+    }
+
+    public void update(Section section) {
+        SparseArray<Double> assignmentWeights = section.getAssignmentWeights();
+        SparseArray<Double> scores            = section.getScores();
+        SparseArray<Double> maxScores         = section.getMaxScores();
+
+        String scoreString;
+
+        scoreString = section.scoreToString(scores.get(Section.HOMEWORK),
+                maxScores.get(Section.HOMEWORK), Section.HOMEWORK);
+        scoreHomework.setText(scoreString + "/" + assignmentWeights.get(Section.HOMEWORK) + "%");
+
+        scoreString = section.scoreToString(scores.get(Section.QUIZ),
+                maxScores.get(Section.QUIZ), Section.QUIZ);
+        scoreQuiz.setText(scoreString + "/" + assignmentWeights.get(Section.QUIZ) + "%");
+
+        scoreString = section.scoreToString(scores.get(Section.MIDTERM),
+                maxScores.get(Section.MIDTERM), Section.MIDTERM);
+        scoreMidterm.setText(scoreString + "/" + assignmentWeights.get(Section.MIDTERM) + "%");
+
+        scoreString = section.scoreToString(scores.get(Section.FINAL),
+                maxScores.get(Section.FINAL), Section.FINAL);
+        scoreFinal.setText(scoreString + "/" + assignmentWeights.get(Section.FINAL) + "%");
+
+        scoreString = section.scoreToString(scores.get(Section.PROJECT),
+                maxScores.get(Section.PROJECT), Section.PROJECT);
+        scoreProject.setText(scoreString + "/" + assignmentWeights.get(Section.PROJECT) + "%");
+
+        scoreString = section.scoreToString(scores.get(Section.OTHER),
+                maxScores.get(Section.OTHER), Section.OTHER);
+        scoreOther.setText(scoreString + "/" + assignmentWeights.get(Section.OTHER) + "%");
+
+        double scorePercent = section.getTotalScore() / section.getMaxScore() * 100;
+
+        if (Double.isNaN(scorePercent)) {
+            scoreString = "";
+        } else {
+            scoreString = String.format(Locale.getDefault(), "%.2f", scorePercent) + "% " +
+                    section.getGrade();
+        }
+        grade.setText(context.getText(R.string.total) + " " + scoreString);
+    }
 }
